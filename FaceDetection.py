96eaa40f33bbde98bbfeca69be12e0b2e8c3a522
VKPInfiniAI <prakash@infiniai.tech>
2023-02-01 19:34:02 +0530

    Face Detection Module Completed

--- FaceDetection.py
+++ FaceDetection.py
@@ -2,7 +2,7 @@ import cv2
 import mediapipe as mp
 import time
 
-cap = cv2.VideoCapture("2.mp4")
+cap = cv2.VideoCapture("Videos/1.mp4")
 pTime = 0
 
 mpFaceDetection = mp.solutions.face_detection
@@ -26,14 +26,14 @@ while True:
             ih, iw, ic = img.shape
             bbox = int(bboxC.xmin * iw), int(bboxC.ymin * ih), \
                    int(bboxC.width * iw), int(bboxC.height * ih)
-            cv2.rectangle(img, bbox, (255,0,255), 2)
-            cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 2)
+            cv2.rectangle(img, bbox, (255,0,255), 5)
+            cv2.putText(img, f'{int(detection.score[0]*100)}%', (bbox[0],bbox[1]-20), cv2.FONT_HERSHEY_PLAIN, 5, (255,0,255), 5)
 
     
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_PLAIN, 3, (0,255,0), 2)
+    cv2.putText(img, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_PLAIN, 5, (0,255,0), 5)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
